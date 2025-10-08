6386e8476dcf53c5792f7fc2214b9a3ac5579edb
arbody <arbody.arb@gmail.com>
2025-10-08 23:10:05 +0300

    Done all tasks

--- 5-3-react-event-handling/src/components/TaskList.jsx
+++ 5-3-react-event-handling/src/components/TaskList.jsx
@@ -5,9 +5,14 @@ export default function TaskList({ tasks, onDelete }) {
   return (
     <ul className="list">
       {/* Task 2 – Display Placeholder if No Tasks Yet */}
+      {(!tasks || tasks.length === 0) && (
+        <li className="placeholder">No tasks yet — add some!</li>
+      )}
 
       {/* Task 2 & 3 – Map tasks to TaskItem */}
-      
+      {tasks && tasks.map((t) => (
+        <TaskItem key={t.id} id={t.id} text={t.text} onDelete={onDelete} />
+      ))}
     </ul>
   );
 }
